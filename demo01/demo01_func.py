532ae1882e560abdec2addf1d878d645e9af77aa
lih94 <lih94@users.noreply.github.com>
2020-02-17 21:03:40 +0800

    20200217 1.完成将列表中的字典按行输出字典的value至txt文件，每个value以逗号分隔；
    20200217[2]
    1.新增注释
    2.新增输出目录

--- demo01/demo01_func.py
+++ demo01/demo01_func.py
@@ -5,6 +5,11 @@ from mimesis import Person
 
 
 def init_message(*args):
+    """
+    初始化数据字典
+    :param args: 需要生成的字段名称，目前支持生成person_name,person_address,person_business,person_datetime,根据需要选择即可
+    :return: 返回数据字典
+    """
     init_person = Person('zh')
     init_address = Address('zh')
     init_business = Business('zh')
@@ -30,6 +35,12 @@ def init_message(*args):
 
 
 def create_list(len_list, *args):
+    """
+    生成数据列表
+    :param len_list:需要生成的数据条数
+    :param args: 需要生成的数据字段名称，同init_message函数的args
+    :return:返回数据列表
+    """
     count_list = 0
     data_list = []
     need_mesaage = args
@@ -40,18 +51,27 @@ def create_list(len_list, *args):
 
 
 def export_to_txt(filename, data):
+    """
+    将数据列表输出至txt文件
+    :param filename: txt文件路径及名称
+    :param data: 数据列表名称
+    :return: 返回将原列表中字典转换为列表后的新数据列表
+    """
     file = open(filename, 'w+', encoding='utf-8')
     init_data = []
+    # 将数据字典转换为列表
     for data_tmp in data:
         dist_to_list = []
         for dist_from_data in data_tmp.values():
             dist_to_list.append(dist_from_data.replace(',', ''))
             # 将字典中的逗号替换为空值
         init_data.append(dist_to_list)
+    # 将数据列表输出至txt文件
     for i in range(len(init_data)):
         s = str(init_data[i]).replace('[', '').replace(']', '')
         s = s.replace("'", '') + '\n'
         file.write(s)
+    file.close()
     return init_data
 
 
